d529d4431ad36c3e221b66da318287b6f27b9e07
ThamaraBortolin <thamaralbortolin@gmail.com>
2022-05-21 19:07:54 -0300

    Updates

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes, Navigate} from 'react-router-dom';
+import Login from  './pages/login/login.jsx';
+import Register from './pages/register/register.jsx'
+
+function App (){
+  return (
+    <BrowserRouter>
+      <Routes>
+      <Route  exact path="/login" element={<Login/>}/>
+      <Route path="/register" element={<Register />}/>
+      <Route path="/" element={<Navigate replace to="/login" />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
