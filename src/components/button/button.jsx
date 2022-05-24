a6d5aac0eb9bc911128feb1e5ee5669a3bd0434b
ThamaraBortolin <thamaralbortolin@gmail.com>
2022-05-24 18:08:10 -0300

    pagina register

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -2,7 +2,7 @@ import style from './style.module.css'
 
 function Button(props){
     return (
-        <button type={props.type} className={style.btn}>{props.textBtn}</button>
+        <button type={props.type} className={style.btn} onClick={props.Click}>{props.textBtn}</button>
     )
 }
 
