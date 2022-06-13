2fd456e04e58ee6187b1aaf02a299f46dbef9a97
ThamaraBortolin <thamaralbortolin@gmail.com>
2022-06-13 20:31:17 -0300

    add itens para carrinho

--- src/components/counter/counter.jsx
+++ src/components/counter/counter.jsx
@@ -1,46 +1,22 @@
 import React from "react";
-import { useState } from 'react';
 import style from './style.module.css'
 
 
-function Counter() {
-
-    let [counter, setCounter] = useState(0)
-
-
-    const decrement = () => {
-        let newQtd = counter - 1
-        if (newQtd <= 0) {
-            setCounter(0)
-        } else {
-            setCounter(newQtd)
-        }
-    }
-
-    const increment = () => {
-        let newQtd = counter + 1
-        setCounter(newQtd)
-    }
+function Counter(props) { 
 
     return (
         <>
+            {props.counter}
             <button
                 type="button"
-                onClick={decrement}
-                className={style.btnCounterMinus}>
-                -
-            </button>
-            <p className={style.qtd}>
-            {counter}
-            </p>
-            <button
-                type="button"
-                onClick={increment}
+                onClick={props.increment}
                 className={style.btnCounterPlus}>
                 +
             </button>
         </>
     )
 }
+
+
 
 export default Counter 
--- src/components/modal/newOrder.jsx
+++ src/components/modal/newOrder.jsx
@@ -1,13 +1,26 @@
 import style from './style.module.css'
 import { IoIosCloseCircle } from 'react-icons/io'
 
-const ModalNewOrder = (props) =>{
+const ModalNewOrder = ({onClick,itens}) =>{
+    
+
     return (
     <section className={style.sectionNewOrder}>
         <form className={style.modalNewOrder}>
-            <IoIosCloseCircle className={style.closeBtn} size="30px" onClick={props.onClick}/>
+            <IoIosCloseCircle className={style.closeBtn} size="30px" onClick={onClick}/>
             <h1 className={style.newOrderTitle}>Novo Pedido</h1>
             <input type="text" name="client" placeholder="Nome do cliente" className={style.inputClientName} required />
+            {itens.map((element)=>
+             (
+                <li key={element.id}>
+                {element.quantity}
+                {element.name}
+                <p>{element.flavor}</p>
+                <p>{element.complement}</p>
+                <p>R${element.price}</p>
+                </li>
+                )
+            )}
         </form>
     </section>
     )
